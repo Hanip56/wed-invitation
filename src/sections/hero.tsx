35befd310d7d95df110ad5c636c834c79607f636
hanip303 <hanipalhapidz56@gmail.com>
2025-04-30 23:24:56 +0700

    Fix some warning eslint

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import FadeInScrollWrapper from "../fadeInScrollWrapper";
 
 const Hero = () => {
--- src/sections/popup.tsx
+++ src/sections/popup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Popup = ({ handleOpen }: { handleOpen: () => void }) => {
